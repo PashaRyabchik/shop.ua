084e3ea105864f6bc07e856954a17569bbbbc09e
Pavel Ryabchikov <barenuk@gmail.com>
2018-07-28 17:43:24 +0300

    update

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,10 +2,12 @@
 
 namespace app\controllers;
 
-class MainController {
+class MainController extends AppController {
 
     public function indexAction(){
-        echo __METHOD__;
+        //echo __METHOD__;
+        $this->setMeta('Главная страница', 'Описание', 'Ключевики...');
+        $this->set(['name' => 'Pasha', 'age' => 31]);
     }
 
 }
