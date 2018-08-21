8b0945c9b6d1f1e4f2233605827a7f0645fca393
Pavel Ryabchikov <barenuk@gmail.com>
2018-08-21 18:37:33 +0300

    update

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,11 +8,8 @@ class MainController extends AppController {
 
     public function indexAction(){
 
-        $posts = \R::findAll('test');
-
         $this->setMeta('Главная страница', 'Описание', 'Ключевики...');
 
-        $this->set(['name' => 'Pasha', 'age' => 31, 'posts' => $posts]);
     }
 
 }
